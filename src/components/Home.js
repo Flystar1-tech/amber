b09dfcae6984ceadb636e89e322777fa9af3f9c7
Flyness <pumulonamatama@gmail.com>
2025-03-24 17:40:24 -0500

    Changed company name to AmbER

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
             href="#"
             style={{ color: "#FFBF00", fontWeight: "bold", fontSize: "1.5rem" }}
           >
-            Amber Accessories
+            AmbER
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
